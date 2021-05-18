3f027839cd092d57b5646f127df47412f3db3d42
victormdev <victoralexandrino99@gmail.com>
2021-05-17 21:56:09 -0300

    update trabalho1

---
+++ Trabalho 1/VictorMacedo2018216039.h
@@ -0,0 +1,31 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome: Victor Macedo Alexandrino dos Santos
+//  email: vmadevops@gmail.com
+//  Matrícula: 2018216039
+//  Semestre: 2021.1
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 20/06/2018 - 19/08/2016
+
+// #################################################
+
+int somar(int x, int y); //função utilizada para testes
+int fatorial(int x); //função utilizada para testes
+int q1(char *data);
+int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtdAnos);
+int q3(wchar_t *str, wchar_t c, int isCaseSensitive);
+int q4(wchar_t *strTexto, wchar_t *strBusca, int posicoes[30]);
+int q5(int num);
+int q6(int numerobase, int numerobusca);
